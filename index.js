b9eee5ea2e866daa28307b277886dd955ee7f55f
Alex <alexadrian2001@yahoo.com>
2023-03-05 18:33:59 +0100

    add drawing 2.0

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let isPainting = false;
+/*let isPainting = false;
 function paintMode() {
-    const body = document.querySelector('html');
+    const body = document.querySelector('.sketch');
     if(isPainting===false){
         isPainting=true
     }else isPainting = false
@@ -14,5 +14,29 @@ function paintMode() {
         body.appendChild(dot);
       }
     });
-  }
+  }*/
 
+  function paintMode() {
+    const body = document.querySelector('.sketch');
+    let isPainting = false;
+
+    body.style.display = "block";
+  
+    body.addEventListener('mousedown', function() {
+      isPainting = true;
+    });
+  
+    body.addEventListener('mouseup', function() {
+      isPainting = false;
+    });
+  
+    body.addEventListener('mousemove', function(event) {
+      if (isPainting) {
+        const dot = document.createElement('div');
+        dot.className = 'paint-dot';
+        dot.style.left = (event.pageX - 4) + 'px';
+        dot.style.top = (event.pageY - 4) + 'px';
+        body.appendChild(dot);
+      }
+    });
+  }
