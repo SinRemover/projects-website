27840f4adc8bf2012a33379f7deda8974b332099
Alex <alexadrian2001@yahoo.com>
2023-03-05 18:12:32 +0100

    add functionality

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+let isPainting = false;
+function paintMode() {
+    const body = document.querySelector('html');
+    if(isPainting===false){
+        isPainting=true
+    }else isPainting = false
+  
+    body.addEventListener('mousemove', function(event) {
+      if (isPainting) {
+        const dot = document.createElement('div');
+        dot.className = 'paint-dot';
+        dot.style.left = (event.pageX - 4) + 'px';
+        dot.style.top = (event.pageY - 4) + 'px';
+        body.appendChild(dot);
+      }
+    });
+  }
 
