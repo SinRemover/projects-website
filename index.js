af2aab59c6a239c246a202d44025362aa0573abd
Alex <alexadrian2001@yahoo.com>
2023-03-06 23:28:58 +0100

    add mobile support

--- index.js
+++ index.js
@@ -1,42 +1,57 @@
-/*let isPainting = false;
 function paintMode() {
-    const body = document.querySelector('.sketch');
-    if(isPainting===false){
-        isPainting=true
-    }else isPainting = false
-  
-    body.addEventListener('mousemove', function(event) {
-      if (isPainting) {
-        const dot = document.createElement('div');
-        dot.className = 'paint-dot';
-        dot.style.left = (event.pageX - 4) + 'px';
-        dot.style.top = (event.pageY - 4) + 'px';
-        body.appendChild(dot);
-      }
-    });
-  }*/
+  const body = document.querySelector('.sketch');
+  let isPainting = false;
 
-  function paintMode() {
-    const body = document.querySelector('.sketch');
-    let isPainting = false;
+  body.style.display = "block";
 
-    body.style.display = "block";
-  
-    body.addEventListener('mousedown', function() {
-      isPainting = true;
-    });
-  
-    body.addEventListener('mouseup', function() {
-      isPainting = false;
-    });
-  
-    body.addEventListener('mousemove', function(event) {
-      if (isPainting) {
-        const dot = document.createElement('div');
-        dot.className = 'paint-dot';
-        dot.style.left = (event.pageX - 4) + 'px';
-        dot.style.top = (event.pageY - 4) + 'px';
-        body.appendChild(dot);
-      }
-    });
-  }
+  body.addEventListener('mousedown', function () {
+    isPainting = true;
+  });
+
+  body.addEventListener('mouseup', function () {
+    isPainting = false;
+  });
+
+  body.addEventListener('mousemove', function (event) {
+    if (isPainting) {
+      const dot = document.createElement('div');
+      dot.className = 'paint-dot';
+      dot.style.left = (event.pageX - 4) + 'px';
+      dot.style.top = (event.pageY - 4) + 'px';
+      body.appendChild(dot);
+    }
+  });
+}
+
+function paintModeMobile() {
+  const body = document.querySelector('.sketch');
+  let isPainting = false;
+
+  body.style.display = "block";
+
+  body.addEventListener('touchstart', function (event) {
+    isPainting = true;
+    const touch = event.touches[0];
+    const dot = document.createElement('div');
+    dot.className = 'paint-dot';
+    dot.style.left = (touch.pageX - 4) + 'px';
+    dot.style.top = (touch.pageY - 4) + 'px';
+    body.appendChild(dot);
+  });
+
+  body.addEventListener('touchend', function () {
+    isPainting = false;
+  });
+
+  body.addEventListener('touchmove', function (event) {
+    if (isPainting) {
+      event.preventDefault();
+      const touch = event.touches[0];
+      const dot = document.createElement('div');
+      dot.className = 'paint-dot';
+      dot.style.left = (touch.pageX - 4) + 'px';
+      dot.style.top = (touch.pageY - 4) + 'px';
+      body.appendChild(dot);
+    }
+  });
+}
